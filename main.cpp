8c3d841f6ddc8a335b8e65843d2efd29a5e36b93
Maggie Voigts <142745784+magsdal3na@users.noreply.github.com>
2026-09-10 21:44:13 -0500

    Update main.cpp
    Fully added best-fit and worst-fit. Output is now correct. Working on setting up function for wait times to handle allocation after jobs are freed up better.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -66,11 +67,13 @@ void freeJob(vector<int>& memory, int jobID) {
     cout << "Freeing job " << jobID << " complete." << endl;
 }
 
+//Function to find Best-Fit
 int findBestFit(const vector<int>& memory, int requiredSize) {
+
     //Initialize int variable that shows free slots
     int freeCount = 0;
-    int bestSize{};
-    int bestStart{};
+    int bestSize = 0;
+    int bestStart = -1;
 
     //Checking to see if enough memory is available for the required size
     for (int i = 0; i < memory.size(); i++) {
@@ -79,6 +82,7 @@ int findBestFit(const vector<int>& memory, int requiredSize) {
             freeCount++;
         }
 
+        //Checking if the fit is the best size and start
         else {
             if(freeCount >= requiredSize && (bestSize == 0 || freeCount < bestSize)) {
                 bestSize = freeCount;
@@ -89,17 +93,161 @@ int findBestFit(const vector<int>& memory, int requiredSize) {
         }
     }
 
+    //Checks for free space at the end of memory
     if(freeCount >= requiredSize && (bestSize == 0 || freeCount < bestSize)) {
         bestSize = freeCount;
         bestStart = memory.size() - freeCount;
     }
 
-    if(bestSize > 0) {
-        return bestStart;
+    return bestStart; 
+}
+
+//Function to find Worst-Fit
+int findWorstFit(const vector<int>& memory, int requiredSize) {
+
+    //Initializing variables for use in the function
+    int freeCount = 0;
+    int worstSize = 0;
+    int worstStart = -1;
+
+    //For loop to check the size of memory
+    for(int i = 0; i < memory.size(); i++) {
+
+        //If memory is empty, increase freeCount
+        if(memory[i] == 0) {
+            freeCount++;
+        }
+
+        //If the memory isn't empty, check for fit
+        else {
+            if(freeCount >= requiredSize && freeCount > worstSize) {
+                worstSize = freeCount;
+                worstStart = i - freeCount;
+            }
+
+            freeCount = 0;
+        }
     }
-    else {
-        return -1;
-    } 
+
+    //Check free space at the end of memory
+    if(freeCount >= requiredSize && freeCount > worstSize) {
+
+        worstSize = freeCount;
+        worstStart = memory.size() - freeCount;
+    }
+
+    return worstStart;
+}
+
+//Struct to represent a completion event to help 
+//calculate completionTime
+struct Event {
+    int time;
+    int jobId;
+};
+
+//Function to allocate a job
+bool allocateJob(vector<int>& memory, const Job& job, int location) {
+
+    if(location == -1) {
+        return false;
+    }
+
+    for(int i = location; i < location + job.size; i++) {
+        memory[i] = job.id;
+    }
+
+    return true;
+}
+
+//Function to choose the allocation strategy
+int findLocation(const vector<int>& memory, int requiredSize, string strategy) {
+
+    //Returns if the strategy is "First-Fit"
+    if(strategy == "First-Fit") {
+        return findFirstFit(memory, requiredSize);
+    }
+
+    //Returns if strategy is "Best-Fit"
+    if(strategy == "Best-Fit") {
+        return findBestFit(memory, requiredSize);
+    }
+
+    //Returns if strategy is "Worst-Fit"
+    if(strategy == "Worst-Fit") {
+        return findWorstFit(memory, requiredSize);
+    }
+
+    return -1;
+}
+
+//Simulation function to replace original loops in main
+//This will make it easier to run repeated simulations without
+//bogging down the program
+void simulate(const vector<Job>& jobs, string strategy) {
+
+    //Setting up vector for memory
+    vector<int> memory(20,0);
+
+    //Output text
+    cout << "\n====================================\n";
+    cout << strategy << " Simulation\n";
+    cout << "\n====================================\n";
+
+    //Keep track of jobs currently in memory
+    vector<Event> events;
+
+    //Go through jobs in the order they appear in jobs.txt
+    for(const Job& job : jobs) {
+
+        int currentTime = job.startTime;
+
+        //Output text with variables to print
+        cout << "\nTime " << job.startTime << endl;
+
+        //Frees jobs whose intervals have ended
+        for(int i = 0; i < events.size(); i++) {
+            if(events[i].time <= currentTime) {
+                cout << "Job " << events[i].jobId
+                     << " interval ended." << endl;
+
+                     freeJob(memory, events[i].jobId);
+
+                     //Remove this event
+                     events.erase(events.begin() + i);
+                     i--;
+            }
+        }
+
+        cout << "Job " << job.id << " arrives." << endl;
+
+        //Setting up location int to work with findLocation function
+        int location = findLocation(memory, job.size, strategy);
+
+        if(location == -1) {
+            cout << "Not enough contiguous memory for Job "
+                 << job.id << "." << endl;
+        }
+
+        else {
+            allocateJob(memory, job, location);
+
+            cout << "Job " << job.id
+                 << " allocated starting at page "
+                 << location << endl;
+
+            //Calculate when the job finishes
+            Event event;
+
+            event.time = job.startTime + job.interval;
+            event.jobId = job.id;
+
+            events.push_back(event);
+        }
+
+        printMemory(memory);
+
+    }
 }
 
 int main() {
@@ -107,15 +255,12 @@ int main() {
     //Vector for jobs
     vector<Job> jobs;
 
-    //Vector for memory
-    vector<int> memory(20, 0);
-
     //Initializing variables (empty)
-    int id{};
-    int startTime{};
-    int size{};
-    int interval{};
-    string endState{};
+    int id;
+    int startTime;
+    int size;
+    int interval;
+    string endState;
 
     //Starting text to show program is booted/working
     cout << "Memory simulation starting..." << endl;
@@ -145,65 +290,20 @@ int main() {
         jobs.push_back(job);
     }
 
+    sort(jobs.begin(), jobs.end(), [](const Job& a, const Job& b) {
+        return a.startTime < b.startTime;
+    });
+
     inputFile.close();
 
     //Printed output
     cout << "Jobs loaded: " << jobs.size() << endl;
 
-    //Looping through jobs and printing variable outputs
-    for (Job job : jobs) {
-        cout << "Job " << job.id
-             << " starts at " << job.startTime
-             << ", requires " << job.size
-             << " KB, interval " << job.interval
-             << ", state " << job.endState
-             << endl;
-
-
-            //First-Fit
-        int location = findBestFit(memory, job.size);
-
-        if (location == -1) {
-        cout << "Not enough contiguous memory." << endl;
-        }
-        else {
-            for (int i = location; i < location + job.size; i++) {
-                memory[i] = job.id;
-        }
-    }
-    }
-
-    printMemory(memory);
-
-    freeJob(memory, 2);
-
-    printMemory(memory);
-
-        //Looping through jobs and printing variable outputs
-    for (Job job : jobs) {
-        cout << "Job " << job.id
-             << " starts at " << job.startTime
-             << ", requires " << job.size
-             << " KB, interval " << job.interval
-             << ", state " << job.endState
-             << endl;
-
-
-            //First-Fit
-        int location = findBestFit(memory, job.size);
-
-        if (location == -1) {
-        cout << "Not enough contiguous memory." << endl;
-        }
-        else {
-            for (int i = location; i < location + job.size; i++) {
-                memory[i] = job.id;
-        }
-    }
-
-    printMemory(memory);
-    
-    }
+    //Runs each allocation strategy based on
+    //functions created above
+    simulate(jobs, "First-Fit");
+    simulate(jobs, "Best-Fit");
+    simulate(jobs, "Worst-Fit");
 
     return 0;
 }
